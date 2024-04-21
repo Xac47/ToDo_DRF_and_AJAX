548ed86ab13bb47b74ee7019a79abd76ab94f158
Xac47 <hasbulat69@gmail.com>
2024-04-21 13:50:46 +0300

    add func list detail create update delete

--- task/views.py
+++ task/views.py
@@ -9,10 +9,53 @@ from .serializers import TasksSerializer
 @api_view(['GET'])
 def apiOverview(request):
     api_urls = {
-        'List': '/task-list/',
+        'List': '/tasks-list/',
         'Detail View': '/task-detail/<str:pk>/',
         'Create': '/task-create/',
         'Update': '/task-update/<str:pk>/',
         'Delete': '/task-delete/<str:pk>/',
     }
     return Response(api_urls)
+
+
+@api_view(['GET'])
+def tasks_list(request):
+    tasks = Tasks.objects.all()
+    serializer = TasksSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def task_detail(request, pk):
+    task = Tasks.objects.get(id=pk)
+    serializer = TasksSerializer(task, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def task_create(request):
+    serializer = TasksSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return (Response(serializer.data))
+
+
+@api_view(['POST'])
+def task_update(request, pk):
+    task = Tasks.objects.get(id=pk)
+    serializer = TasksSerializer(instance=task, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def task_delete(request, pk):
+    task = Tasks.objects.get(id=pk)
+    task.delete()
+
+    return Response('Task was deleted!')
