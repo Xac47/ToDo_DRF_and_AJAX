eaa06fd68d13b4f3fe9dd51c27e0564419723114
Xac47 <hasbulat69@gmail.com>
2024-04-21 13:51:37 +0300

    add urls list detail create update delete

--- task/urls.py
+++ task/urls.py
@@ -1,3 +1,11 @@
 from django.urls import path
+from . import views
 
-
+urlpatterns = [
+    path('', views.apiOverview, name='api_overview'),
+    path('tasks-list/', views.tasks_list, name='tasks_list'),
+    path('task-detail/<str:pk>/', views.task_detail, name='task_detail'),
+    path('task-create/', views.task_create, name='task_create'),
+    path('task-update/<str:pk>/', views.task_update, name='task_update'),
+    path('task-delete/<str:pk>/', views.task_delete, name='task_delete'),
+]
